504501fd7a61e02bdabda8d71f0dcc0efdc0333b
hweiyu <hwy_dod@163.com>
2018-06-12 11:42:08 +0800

    push resource code

---
+++ src/main/java/com/hwy/SpringBootWarApplication.java
@@ -0,0 +1,12 @@
+package com.hwy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWarApplication.class, args);
+    }
+}
---
+++ src/main/java/com/hwy/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.hwy.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author huangweiyu
+ * @version V1.0
+ * @Title: 描述
+ * @Description: 描述
+ * @date 2018/6/12 11:29
+ **/
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "helle index";
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello, welcome";
+    }
+}
